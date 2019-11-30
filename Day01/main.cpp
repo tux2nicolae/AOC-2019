adf55e2f163ca02ee720754067d7b113a62e1742
Telechi Nicolae <tux2nicolae@gmail.com>
2019-11-30 22:11:46 +0200

    Add initial library for AOC-2019

---
+++ Day01/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Advent of code 2019
+ * @author : Nicolae Telechi
+ */
+#include <iostream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <unordered_map>
+#include <optional>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+  ifstream in("D:\\AOC-2019\\Day1\\Day1.in");
+  ofstream out("D:\\AOC-2019\\Day1\\Day1.out");
+
+  return 0;
+}
