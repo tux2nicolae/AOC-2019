7dec0f884765062e74b5fa60579e05c88e58a062
Nicolae Telechi <nicolae.telechi@caphyon.com>
2019-12-08 09:50:34 +0200

    Prepare projects for remained days

---
+++ Day10/src/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Advent of code 2019
+ * @author : Nicolae Telechi
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <optional>
+#include <numeric>
+
+using namespace std;
+
+#include "../../AOCLib/src/Algorithm.h"
+#include "../../AOCLib/src/FStreamReader.h"
+#include "../../AOCLib/src/FStreamWriter.h"
+#include "../../AOCLib/src/Math.h"
+#include "../../AOCLib/src/Time.h"
+
+int main()
+{
+  ifstream in("..\\..\\Day10\\src\\Day10.in");
+  ofstream out("..\\..\\Day10\\src\\Day10.out");
+
+  FStreamReader reader(in);
+  auto v = reader.ReadVector();
+  
+  return 0;
+}
