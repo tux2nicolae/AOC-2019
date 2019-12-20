a110b067bdcbb869bc4871b4e4ceb29fa591a6e8
Nicolae Telechi <nicolae.telechi@caphyon.com>
2019-12-20 09:21:23 +0200

    Full part 1, partial part 2

--- Day20/src/main.cpp
+++ Day20/src/main.cpp
@@ -15,6 +15,8 @@
 #include <unordered_map>
 #include <optional>
 #include <numeric>
+#include <queue>
+
 #include <assert.h>
 
 using namespace std;
@@ -25,15 +27,179 @@ using namespace std;
 #include "../../AOCLib/src/Math.h"
 #include "../../AOCLib/src/Time.h"
 
+struct Node
+{
+  int cost{};
+  AOC::Point position;
+};
+
+vector<string> m;
+
+map<AOC::Point, vector<Node>> graph;
+unordered_map<string, vector<AOC::Point>> portals;
+
+// inner = true
+map<AOC::Point, bool> portalTypes;
+
+bool isUpperLetter(char c)
+{
+  return 'A' <= c && c <= 'Z';
+}
+
+bool isInnerPortal(AOC::Point portal)
+{
+  return portal.x > 1 && portal.y > 1 && portal.x < (m.size() - 1) && (m[0].size() - 1);
+}
+
+string readPortal(const vector<string>& m, AOC::Point from)
+{
+  static const int directionX[4] = { -1,  0, 1, 0 };
+  static const int directionY[4] = { 0,  1, 0, -1 };
+
+  string portal("**");
+
+  // up
+  if (isUpperLetter(m[from.x - 1][from.y]))
+  {
+    portal[0] = m[from.x - 2][from.y];
+    portal[1] = m[from.x - 1][from.y];
+  }
+  // right
+  else if (isUpperLetter(m[from.x][from.y + 1]))
+  {
+    portal[0] = m[from.x][from.y + 1];
+    portal[1] = m[from.x][from.y + 2];
+  }
+  // down
+  else if (isUpperLetter(m[from.x + 1][from.y]))
+  {
+    portal[0] = m[from.x + 1][from.y];
+    portal[1] = m[from.x + 2][from.y];
+  }
+  // left
+  else if (isUpperLetter(m[from.x][from.y - 1]))
+  {
+    portal[0] = m[from.x][from.y - 2];
+    portal[1] = m[from.x][from.y - 1];
+  }
+
+  assert(portal != "**");
+  return portal;
+}
+
 int main()
 {
-	ifstream in("..\\..\\Day20\\src\\Day20.in");
-	ofstream out("..\\..\\Day20\\src\\Day20.out");
+  ifstream in("..\\..\\Day20\\src\\Day20.in");
+  ofstream out("..\\..\\Day20\\src\\Day20.out");
+  
+  FStreamReader reader(in);
+  m = reader.ReadVectorOfWords();
 
-	// forgot to commit from work pc, right now i'm outside the country, will commit when will return to work
+  AOC::Point entrance;
+  AOC::Point exit;
 
-	FStreamReader reader(in);
-	auto v = reader.ReadVector();
+  // read all neighbours
+  for (int i = 0; i < m.size(); i++)
+  {
+    for (int j = 0; j < m[0].size(); j++)
+    {
+      if (m[i][j] != '.')
+        continue;
 
-	return 0;
+      AOC::Point from = {i, j};
+
+      static const int directionX[4] = { -1,  0, 1, 0 };
+      static const int directionY[4] = { 0,  1, 0, -1 };
+
+      for (int k = 0; k < 4; ++k)
+      {
+        AOC::Point to;
+        to.x = from.x + directionX[k];
+        to.y = from.y + directionY[k];
+
+        if (m[to.x][to.y] == '.')
+        {
+          auto& node = graph[from];
+          node.push_back(Node{1, to});
+        }
+        else if ('A' <= m[to.x][to.y] && m[to.x][to.y] <= 'Z')
+        {
+          graph[from].push_back(Node{1, to});
+          graph[to].push_back(Node{0, from});
+
+          string portal = readPortal(m, from);
+
+          if (portal == "AA")
+            entrance = to;
+          else if (portal == "ZZ")
+            exit = to;
+          else
+          {
+            portals[portal].push_back(to);
+            portalTypes[to] = isInnerPortal(to);
+          }
+        }
+      }
+    }
+  }
+
+  // read all portals
+  for (auto& [portal, portalPositions] : portals)
+  {
+    assert(portalPositions.size() == 2);
+
+    graph[portalPositions[0]].push_back(Node{0, portalPositions[1]});
+    graph[portalPositions[1]].push_back(Node{0, portalPositions[0]});
+  }
+
+  struct Cost
+  {
+    int cost = numeric_limits<int>::max();
+  };
+
+  struct ID{
+    AOC::Point value;
+    int level = 0;
+  };
+
+  map<AOC::Point, unordered_map<int, Cost>> costs;
+  map<AOC::Point, unordered_map<int, bool>> visited;
+
+  // run dijkstra
+  auto heapComparator = [&](ID& left, ID& right) { 
+    return costs[left.value][left.level].cost > costs[right.value][right.level].cost; 
+  };
+
+  std::priority_queue<ID, vector<ID>, decltype(heapComparator)> heap(heapComparator);
+
+  heap.push(ID{ entrance, 0 });
+  costs[entrance][0] = { 0 };
+
+  while (!heap.empty())
+  {
+    auto current = heap.top();
+    heap.pop();
+
+    if (current.level == 0 && current.value == exit)
+    {
+      cout << costs[current.value][current.level].cost - 1;
+      break;
+    }
+
+    visited[current.value][current.level] = true;
+
+    for (auto& neighbour : graph[current.value])
+    {
+      if (visited[neighbour.position][current.level])
+        continue;
+
+      if (costs[neighbour.position][current.level].cost > costs[current.value][current.level].cost + neighbour.cost)
+      {
+        costs[neighbour.position][current.level] = Cost{costs[current.value][current.level].cost + neighbour.cost};
+        heap.push(ID{ neighbour.position, current.level });
+      }
+    }
+  }
+
+  return 0;
 }
